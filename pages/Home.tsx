1c42ceb388088c2b772061a660b2b2077a3ac292
Ran Hassid <ran.hassid@gmail.com>
2023-01-04 14:18:03 +0200

    add material design (#6)

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,20 +1,22 @@
 import { View, Text, StyleSheet } from 'react-native';
+import { Button } from '@react-native-material/core';
 
 const Home = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <Text>Home page</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Home page</Text>
+      <Button title="Material design button"></Button>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
 
 export default Home;
