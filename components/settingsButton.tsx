4c35de75438fc9721d49297f841228eac5375650
Tomer Epstein <tomer.epstein@sap.com>
2023-01-05 15:43:20 +0200

    setting icon

--- components/settingsButton.tsx
+++ components/settingsButton.tsx
@@ -1,14 +1,16 @@
 import { StyleSheet, View, Text, Button, Image, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import * as React from 'react';
+import { MdSettings } from "react-icons/md";
 
 function MyButton({ }) {
     const navigation = useNavigation();
 
     return (
-        <Button
-            title="Settings"
-            onPress={() => navigation.navigate('Settings')}
+        <MdSettings
+            title="Settings" 
+            onClick={() => navigation.navigate('Settings')}
+            size={30}
         />
     );
 }
