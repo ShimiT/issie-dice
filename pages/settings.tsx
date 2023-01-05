0ce2bd9715b7a331660cf3b282bbcb6a8d3d8564
Yoni Romm <yonatan.romm@sap.com>
2023-01-05 13:43:14 +0200

    update settings layout

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,11 +1,9 @@
-import { Box, Text, Button } from '@react-native-material/core';
+import { Box, Text, Button, Flex } from '@react-native-material/core';
 import { StyleSheet, View } from 'react-native';
 import React, { useState, Component } from 'react';
 import TimeButtons from '../components/timeButton';
 import CubeCounter from '../components/cubesCounter';
 import OnOffLayer from '../components/onOffLayer';
-
-
 
 const styles = StyleSheet.create({
     container: {
@@ -16,26 +14,32 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'space-between',
-        marginTop: 100,
+        // marginTop: 100,
     },
     text: {
+        marginTop: 50,
         color: '#BABABA',
-
-    }
-
+    },
 });
 
 const Settings = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.text}> Design Your Game</Text>
+            <Flex style={{ alignItems: 'center' }}>
+                <Text style={styles.text}> Num of Cubes</Text>
+                <Flex>
+                    {<CubeCounter></CubeCounter>}
+                </Flex>
+            </Flex>
             {/* <Image
                 style={styles.tinyLogo}
                 source={require('@expo/snack-static/react-native-logo.png')}
             ></Image> */}
-            {<CubeCounter></CubeCounter>}
-            {<OnOffLayer></OnOffLayer>}
-            {<TimeButtons></TimeButtons>}
+            <Flex>
+                {<OnOffLayer></OnOffLayer>}
+                {<TimeButtons></TimeButtons>}
+            </Flex>
         </View>
     );
 };
