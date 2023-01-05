fcb3b1eb6119f2761158867800b16f426bb44dbb
Tomer Epstein <tomer.epstein@sap.com>
2023-01-05 13:42:14 +0200

    react-native-switch

--- components/onOffButton.tsx
+++ components/onOffButton.tsx
@@ -1,5 +1,6 @@
 import { Box, Text, Button } from '@react-native-material/core';
-import { View, Switch, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Switch } from 'react-native-switch';
 import React, { useState } from 'react';
 
 const onOffButton = () => {
@@ -10,12 +11,15 @@ const onOffButton = () => {
         <View style={styles.container}>
             <Switch
                 // trackColor={{ false: "#767577", true: "#81b0ff" }}
-                trackColor={{ true: 'green', false: 'grey' }}
-                thumbColor="#fff"
+                backgroundActive={'green'}
+                backgroundInactive={'gray'}
+                // thumbColor="#fff"
                 // thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-                ios_backgroundColor="#3e3e3e"
+                // ios_backgroundColor="#3e3e3e"
                 onValueChange={toggleSwitch}
                 value={isEnabled}
+                activeText={'On'}
+                inActiveText={'Off'}
             />
         </View>
     );
@@ -23,17 +27,21 @@ const onOffButton = () => {
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: '#1A191B',
         flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+        borderRadius: 15,
     },
     switch: {
         transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }],
     },
     text: {
         fontSize: 18,
-        marginRight: 10,
+        marginRight: 20,
     },
 });
 
