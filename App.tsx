55234c8d573feffcfe302f22d0282e6dcb9d6a39
Jacob Shalom <jacob.shalom@sap.com>
2023-01-05 15:55:05 +0200

    add support for global store

--- App.tsx
+++ App.tsx
@@ -1,33 +1,46 @@
 import { StyleSheet, View, Text, Button, Image, TouchableOpacity } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useState } from 'react';
-
+import { GlobalStoreProvider } from "react-native-global-store";
 const StackNav = createNativeStackNavigator();
 const { Navigator, Screen } = StackNav;
 
 const Stack = createNativeStackNavigator();
 
 import Home from './pages/Home';
-import Settings from './pages/settings'
 import SettingsButton from './components/settingsButton'
+import Settings from "./pages/settings";
+
+const initialState = {
+    count: 1,
+    recoveryTime: 10,
+    size: 'M',
+};
+
+const persistedKeys = ["count","recoveryTime","size"];
+
+
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Navigator>
-        <Stack.Screen
-          name="home"
-          component={Home}
-          options={{
+      <GlobalStoreProvider
+          initialState={initialState}
+          persistedKeys={persistedKeys}
+      ><NavigationContainer>
+              <Navigator>
+                  <Stack.Screen
+                      name="home"
+                      component={Home}
+                      options={{
             headerRight: () => (
               <SettingsButton></SettingsButton>
             ),
-            title: 'Home'
-          }}
-        ></Stack.Screen>
+                          title: 'Home'
+                      }}
+                  ></Stack.Screen>
         <Stack.Screen name="Settings" component={Settings} />
-      </Navigator>
-    </NavigationContainer>
+              </Navigator>
+          </NavigationContainer>
+      </GlobalStoreProvider>
   );
 }
